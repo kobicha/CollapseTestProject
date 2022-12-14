edbb0d044b50c17d9dc85c2d73af9d00aec1391c
Kobi <kobi.chariski@gmail.com>
2022-12-14 13:48:13 +0200

    added a GridPosition class to better handle cordinates

---
+++ Assets/Scripts/Collapse/GridPosition.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Collapse
+{
+    public struct GridPosition : IEquatable<GridPosition>
+    {
+        public int x;
+        public int y;
+        
+        public GridPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public override bool Equals(object obj)
+        {
+            return obj is GridPosition other && Equals(other);
+        }
+        
+        public bool Equals(GridPosition other)
+        {
+            return x == other.x && y == other.y;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (x * 397) ^ y;
+            }
+        }
+
+        public static bool operator == (GridPosition leftSide, GridPosition rightSide)
+        {
+            return leftSide.x == rightSide.x && leftSide.y == rightSide.y;
+        }
+
+        public static bool operator != (GridPosition leftSide, GridPosition rightSide)
+        {
+            return !(leftSide == rightSide);
+        }
+        
+        public static GridPosition operator+(GridPosition a, GridPosition b)
+        {
+            return new GridPosition(a.x + b.x, a.y + b.y);
+        }
+
+        public static GridPosition operator-(GridPosition a, GridPosition b)
+        {
+            return new GridPosition(a.x - b.x, a.y - b.y);
+        }
+    }
+}
